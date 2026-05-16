1cf612a1e894fb0179cb362e50be920efe05b2c2
Klaudia0212 <klaudia.scigalska@gmail.com>
2026-05-16 10:46:39 +0200

    reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  let email = 'klaudia@gmail.com';
+  const [email, setEmail] = useState('fracz@agh.edu.pl');
 
-  function handleChange(event){
-    console.log(event.target.value);
-  }
+    function handleChange(event) {
+        setEmail(event.target.value);
+    }
+
+    let message;
+    if(email.length < 5){
+      message = <div>za krótki adres!</div>;
+    } else if (email.length < 15){
+      message = <div>dobry adres email</div>;
+    } else {
+      message = <div>za długi adres email</div>;
+    }
+  
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój email to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            {message}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
